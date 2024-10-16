39c461a47ed84410ead4f0e91a5aa9313b0d8990
howonK <hwkwak@mz.co.kr>
2024-10-16 17:47:01 +0900

    test commit

---
+++ src/test/java/com/example/AlgorithmPractice/AlgorithmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlgorithmPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
